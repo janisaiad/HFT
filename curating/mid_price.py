574b656ba376bd472c09310d271be3772eeaf6e4
Janis Aiad <janisaiad.ja@gmail.com>
2025-03-15 17:38:33 +0100

    cleared curating outputs

--- curating/mid_price.py
+++ curating/mid_price.py
@@ -32,9 +32,7 @@ def curate_mid_price(stock, file, folder_path=folder_path):
     mid_price = mid_price.fill_nan(mid_price.shift(1))
     df = df.with_columns(mid_price=mid_price)
     
-
-
-
-
-
-
+    
+    
+    
+    
