f88e8adec077bfcf7507db13ee8ac84bd32a97fe
Janis Aiad <janisaiad.ja@gmail.com>
2025-03-27 10:57:52 +0100

    N dimensionnal coding

--- models/hawkes/hawkes.py
+++ models/hawkes/hawkes.py
@@ -5,25 +5,39 @@ import polars as pl
 import pandas as pd
 
 
-class Hawkes:
-    def __init__(self, phi: callable, psi: callable, mu: float):
+class Hawkes: # N dimensionnal hawkes process
+    def __init__(self, phi: callable, psi: callable, mu: float, N: int):
+        # phi returns a N dimensionnal vector
+        # psi returns a N dimensionnal vector
+        # mu is a N dimensionnal vector
+        self.dim = N
         self.phi = phi
         self.psi = psi
         self.mu = mu
-        self.list_of_events = [] # list of time numpy to use after
+        self.list_of_events = [[] for _ in range(N)] # matrix of time events, of size N x T
         
-    def add_event(self, event: float):
-        self.list_of_events.append(event)
+        
+        
+    def add_event(self, event: float, dimension: int):
+        self.list_of_events[dimension].append(event)
     
     
-    def get_intensity(self, t: float):
+    def get_intensity(self, t: float) -> np.ndarray: # returns a vector of intensities
         return self.mu + sum(self.psi(t - event) for event in self.list_of_events)
+    
+    
+    
+    
+    
+    
     
     
     
     def simulate(self, T: int, n: int):
         pass
 
-    def fit(self, X: np.ndarray):
+
+
+    def fit(self, X: np.ndarray): # X is a numpy array of events
         pass
 
